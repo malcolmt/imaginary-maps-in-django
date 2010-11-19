39e1ad8613c8e2e005a312668aeea323c07837f0
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2010-11-19 22:09:34 +1300

    Pull out some common code.

--- interface/views.py
+++ interface/views.py
@@ -3,20 +3,25 @@ from django.contrib.gis.shortcuts import render_to_kml
 
 import adventure.models
 
-def tracks(request):
+KML_TEMPLATE = "gis/kml/placemarks.kml"
+
+def _get_request_box(request):
+    """
+    Returns a geos.Polygon that describes the OpenLayers bounding box for the
+    request.
+    """
     bbox = request.GET.get("BBOX", request.GET.get("bbox"))
     minx, miny, maxx, maxy = [float(elt) for elt in bbox.split(",")]
-    geom = geos.Polygon.from_bbox((minx, miny, maxx, maxy))
+    return geos.Polygon.from_bbox((minx, miny, maxx, maxy))
+
+def tracks(request):
+    geom = _get_request_box(request)
     result = adventure.models.Track.objects.filter(path__intersects=geom).kml()
-    return render_to_kml("gis/kml/placemarks.kml", {"geomdata": result,
-        "places": result})
+    return render_to_kml(KML_TEMPLATE, {"geomdata": result, "places": result})
 
 def places(request):
-    bbox = request.GET.get("BBOX", request.GET.get("bbox"))
-    minx, miny, maxx, maxy = [float(elt) for elt in bbox.split(",")]
-    geom = geos.Polygon.from_bbox((minx, miny, maxx, maxy))
+    geom = _get_request_box(request)
     result = adventure.models.PointOfInterest.objects. \
             filter(location__bboverlaps=geom).kml()
-    return render_to_kml("gis/kml/placemarks.kml", {"geomdata": result,
-        "places": result})
+    return render_to_kml(KML_TEMPLATE, {"geomdata": result, "places": result})
 
